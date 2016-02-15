9e0c844126e7bfb80d241113982b8feacffe573c
Josh Overly and Mollie Bootsma <student@epicodus.com>
2016-02-15 08:27:47 -0800

    made the thing

---
+++ tests/TitleCaseGeneratorTest.php
@@ -0,0 +1,33 @@
+<?php
+    require_once "src/TitleCaseGenerator.php";
+
+    class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
+    {
+        function test_makeTestTitleCase_oneWord()
+        {
+            //Arrange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "beowolf";
+
+            //Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("Beowolf", $result);
+        }
+
+        function test_makeTestTitleCase_multipleWords()
+        {
+            //Arrange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "the little mermaid";
+
+            //Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("The Little Mermaid", $result);
+        }
+    }
+
+ ?>
